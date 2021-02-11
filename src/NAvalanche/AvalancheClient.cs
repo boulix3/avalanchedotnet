19d65526fa71f5468e601294e0c6384134a1790d
boulix <boulix3@gmail.com>
2021-02-11 21:33:16 +0100

    Initialize and first request

---
+++ src/NAvalanche/AvalancheClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+
+namespace NAvalanche
+{
+    public class AvalancheClient
+    {
+        private HttpClient _http;
+        public AvalancheClient()
+        {
+            _http = new HttpClient();
+        }
+
+        public string InfoGetNodeVersion()
+        {
+            var request = new ApiRequestInfo
+            {
+                jsonrpc = "2.0",
+                id = 1,
+                method = "info.getNodeID"
+            };
+            var httpResult = _http.PostAsJsonAsync("http://box:9650/ext/info", request);
+            var stringResult = httpResult.Result.Content.ReadAsStringAsync().Result;
+            Console.WriteLine("string reuslt : " + stringResult);
+            return stringResult;            
+        }
+    }
+    public class ApiRequestInfo
+    {
+        public string jsonrpc { get; set; }
+        public int id { get; set; }
+        public string method { get; set;}
+        [JsonPropertyName("params")]
+        public Dictionary<string, string> parameters { get; set; } = new();
+}
+}
---
+++ src/Tests/Api/Info.cs
@@ -0,0 +1,35 @@
+﻿using NAvalanche;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Tests.Api
+{
+    public class Info
+    {
+        private readonly ITestOutputHelper output;
+        public AvalancheClient client;
+        public Info(ITestOutputHelper output)
+        {
+            this.output = output;
+            client = new AvalancheClient();
+        }
+        [Fact]
+        public void GetNodeId()
+        {
+
+        }
+
+        [Fact]
+        public void GetNodeVersion()
+        {
+            var result = client.InfoGetNodeVersion();
+            Assert.NotNull(result);
+            output.WriteLine(result);
+        }
+    }
+}
